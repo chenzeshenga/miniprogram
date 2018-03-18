73e2119455ad5ce56d388b06c600b718e324080e
chenzeshenga <chenzeshenga@163.com>
2018-03-18 23:18:15 +0800

    data loader

--- pages/index2/index.js
+++ pages/index2/index.js
@@ -4,74 +4,33 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World, mini program',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    objectArray: [
-      { id: 1, unique: "unique_1" },
-      { id: 4, unique: "unique_4" },
-      { id: 2, unique: "unique_3" },
-    ]
-  },
-  switch: function (e) {
-    const length = this.data.objectArray.length;
-    var arrays = this.data.objectArray;
-    var temp = 0;
-    for (let i = 0; i < length; i++) {
-      for (let j = i + 1; j < length; j++) {
-        if (arrays[i].id < arrays[j].id) {
-          temp = arrays[j];
-          arrays[j] = arrays[i];
-          arrays[i] = temp;
-        }
-      }
+    text: "init data",
+    array: [{ text: "init data" }],
+    object: {
+      text: "init data"
     }
+  },
+  changeText:function(){
+    this.setData(
+      {
+        text:"changed data"
+      }
+    )
+  },
+  changeItemInArray:function(){
     this.setData({
-      objectArray: this.data.objectArray
+      "array[0].text":"changed data"
     })
   },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
+  changeItemInObject: function () {
+    this.setData({
+      "object.text": "changed data"
     })
   },
-  onLoad: function () {
+  addNewFields: function () {
+    this.setData({
+      "newField.text": "new data"
+    })
+  },
 
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
